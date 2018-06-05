a670bcd015b32f4a554fe751ce4e093518d5b3e2
qian <caiqianlucy@gmail.com>
2018-06-04 20:19:17 -0700

    add js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 /*
  * Create a list that holds all of your cards
  */
+let cards=document.getElementsByClassName("card");
+
+
+
+let deck=document.querySelector(".deck");
+//declare move number variable
+let moveNumber=0;
+let counter=document.querySelector(".moves");
+
+//declaring star variable
+let stars=document.querySelectorAll(".fa-star");
+
+
+//decalre matchedcard variable
+let matchedCards=document.getElementsByClassName("match");
+
+//declare opened cards
+let openedCards=[];
+
+//declare restart, playAgain
+let restartGame=document.getElementsByClassName("restart");
+let playAgain=document.getElementById("playAgain");
 
 
 /*
@@ -25,7 +47,139 @@ function shuffle(array) {
     return array;
 }
 
+document.body.onload=startGame();
 
+function startGame(){
+  cards=shuffle(cards);
+  //deck.innerHTML="";
+  for (let i=0; i<cards.length; i++){
+    cards[i].classList.remove("show", "open", "match", "disabled");
+    deck.append(cards[i]);
+  }
+  //reset moveNumber
+  moveNumber=0;
+  counter.innerHTML=moveNumber;
+  //reset star rating;
+  for (var i=0; i<stars.length; i++){
+    stars[i].style.color="ffff00";
+    stars[i].style.visibility="visible";
+  }
+  //reset timer
+  let second=0;
+  let minute=0;
+  let hour=0;
+  let timer=document.querySelector(".timer");
+  timer.innerHTML="0 hrs 0 mins 0 secs";
+  clearInterval(interval);
+}
+
+restartGame.addEventListener("click", startGame);
+playAgain.addEventListener("click", startGame);
+
+//display the card's symbol
+function display(){
+  this.classList.add("open");
+  this.classList.add("show");
+}
+
+//check match or unmatch when open a card
+function openCard(){
+  openedCards.push(this);
+  let len=openedCards.length;
+  if(len===2){
+    moveCounter();
+    if(openedCards[0].children().attr("class")===openedCards[1].children().attr("class")){
+      matched();
+    }
+    else {
+      unmatched();
+    }
+  }
+}
+
+// when two cards are matched
+function matched(){
+  openedCards[0].classList.add("match", "disabled");
+  openedCards[1].classList.add("match", "disabled");
+  openedCards[0].classList.remove("show", "open");
+  openedCards[1].classList.remove("show", "open");
+  openedCards=[];
+}
+
+//when two cards are not matched
+function unmatched(){
+  openedCards[0].classList.add("unmatched");
+  openedCards[1].classList.add("unmatched");
+  Array.prototype.filter.call(cards, function(card){
+    card.clssList.add("disabled")
+  });
+  setTimeout(function(){
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    Array.prototype.filter.call(cards, function(card){
+      card.classList.remove("disabled");
+      for (let i=0; i<matchedCards.length; i++){
+        matchedCard[i].classList.add("disabled");
+      }
+      openedCards=[];
+    });
+  }, 1000);
+}
+
+function moveCounter(){
+  moveNumber++;
+  counter.innerHTML=moveNumber;
+  //start timer on first move;
+  if (moveNumber==1){
+    second=0;
+    minute=0;
+    hour=0;
+    startTimer();
+  }
+  //set star rates based on moves
+  if (moveNumber>12 && moveNumber<16) {
+    stars[2].style.visibility="collapse";
+  }
+  else if (moveNumber>15) {
+    for (i=1; i<3; i++){
+      stars[i].style.visibility="collapse";
+    }
+  }
+}
+
+//timer function
+let timer=document.querySelector(".timer");
+let interval;
+function startTimer(){
+  interval=setInterval(function(){
+    timer.innerHTML=hour+"hrs"+minute+"mins"+second+"secs";
+    second++;
+    if (second==60){
+      minute++;
+      second=0;
+    }
+    if (minute==60){
+      hour++;
+      minute=0;
+    }
+  }, 1000);
+}
+// win message
+function win(){
+  if (matchedCard.length==16){
+    clearInterval(interval);
+    winMemo.classList.add("show");
+    document.getElementById("totalMove").innerHTML=moveNumber;
+    document.getElementById("starRating").innerHTML=document.querySelector("stars").innerHTML;
+  }
+}
+
+//add eventlistener to card
+for (let i=0; i<cards.length; i++){
+  cards[i].addEventListener("click", display);
+  cards[i].addEventListener("click", openCard;
+  cards[i].addEventListener("click", win);
+}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
